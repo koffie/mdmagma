2137432cb4e6003031f8896343bce8b6d0eca0e6
Maarten Derickx <maarten@mderickx.nl>
2024-12-04 17:24:22 +0100

    Add test for MDMultiset

--- tests/v2/test_utils.m
+++ tests/v2/test_utils.m
@@ -18,6 +18,20 @@ procedure test_MDMagmaSourceDir()
   TSTAssertEQ(s[#s] where s := Split(source_dir,"/"), "v2");
 end procedure;
 
+procedure test_MDMultiset()
+  A<x,y> := AffineSpace(GF(2),2);
+  C := Curve(A, x-y);
+  CF4 := RationalPoints(C,GF(4));
+  S := MDMultiset([Places(P)[1] : P in CF4]);
+  // this is the expected behaviour since CF4 contains two GF(2) points and a pair
+  // of galois conjugate points over GF(4)
+  TSTAssertEQ({Multiplicity(S,x) : x in S}, {1,2});
+  S := Multiset([Places(P)[1] : P in CF4]);
+  // here we test the wrong behaviour of magma
+  TSTAssertEQ({Multiplicity(S,x) : x in S}, {1});
+end procedure;
+
 //test_AttributeAsString_assigned();
 test_AttributeAsString_unassigned();
 test_MDMagmaSourceDir();
+test_MDMultiset();
