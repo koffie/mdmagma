2694692ee7df3f62f95a9f57dc0ee93a464a7eeb
Maarten Derickx <maarten@mderickx.nl>
2024-12-06 14:24:44 +0100

    Rename filter to sieve

--- v2/modular_curves/rational_points.m
+++ v2/modular_curves/rational_points.m
@@ -1,26 +1,26 @@
-intrinsic HeckeFilter(X::MDCrvMod, q::RngIntElt, S::SeqEnum) -> SeqEnum
+intrinsic HeckeSieve(X::MDCrvMod, q::RngIntElt, S::SeqEnum) -> SeqEnum
 { Return all the divisors D in S in such that (T_q-q<q>-1)D=0 }
-  filtered := [];
+  sieved := [];
   for D in S do
     A_qD := HeckeOperator(X,q,D)-q*DiamondOperator(X,q,D)-D;
     if IsPrincipal(A_qD) then
-     Append(~filtered, D);
+     Append(~sieved, D);
     end if;
   end for;
-  return filtered;
+  return sieved;
 end intrinsic;
 
-intrinsic HeckeFilter(X::MDCrvMod, Y::MDCrvMod, q::RngIntElt, S::SeqEnum) -> SeqEnum
+intrinsic HeckeSieve(X::MDCrvMod, Y::MDCrvMod, q::RngIntElt, S::SeqEnum) -> SeqEnum
 { Return all the divisors D in S in such that (T_q-q<q>-1)D'=0 where D' is the image
  of D in Y}
-  filtered := [];
+  sieved := [];
   for D in S do
     D1 := DegeneracyMap(X, Y, D);
     time A_qD := HeckeOperator(Y,q,D1)-q*DiamondOperator(Y,q,D1)-D1;
     if IsPrincipal(A_qD) then
      print Index(S,D);
-     Append(~filtered, D);
+     Append(~sieved, D);
     end if;
   end for;
-  return filtered;
+  return sieved;
 end intrinsic;
